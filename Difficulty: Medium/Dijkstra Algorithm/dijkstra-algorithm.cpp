fdf345e3d07c221d6a10c768d60f8cbcf55f821d
Aayush Sharma <137158202+AayushSharma54652@users.noreply.github.com>
2024-08-02 00:02:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
@@ -10,20 +10,25 @@ class Solution
     //from the source vertex S.
     vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
     {
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-        vector<int>dist(V,INT_MAX);
+        set<pair<int,int>>st;
+        vector<int>dist(V,1e9);
         dist[S] = 0;
-        pq.push({0,S});
-        while(!pq.empty()){
-            int dis = pq.top().first;
-            int node = pq.top().second;
-            pq.pop();
+        st.insert({0,S});
+        while(!st.empty()){
+            auto it = *(st.begin());
+            int node = it.second;
+            int dis = it.first;
+            st.erase(it);
             for(auto it:adj[node]){
-                int edgeWeight = it[1];
-                int adjNode = it[0];
-                if(dis + edgeWeight < dist[adjNode]){
-                    dist[adjNode] = dis+edgeWeight;
-                    pq.push({dist[adjNode],adjNode});
+                int edgewt = it[1];
+                int adjnode = it[0];
+                if(dis + edgewt < dist[adjnode]){
+                    //erase if it existed
+                    if(dist[adjnode] != 1e9){
+                        st.erase({dist[adjnode],adjnode});
+                    }
+                    dist[adjnode] = dis + edgewt;
+                    st.insert({dist[adjnode],adjnode});
                 }
             }
         }

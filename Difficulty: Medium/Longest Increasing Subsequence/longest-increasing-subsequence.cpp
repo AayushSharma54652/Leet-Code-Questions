c19d2df638a057135d1d4e0330655469e9bf0782
Aayush Sharma <137158202+AayushSharma54652@users.noreply.github.com>
2024-11-26 16:37:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Increasing Subsequence/longest-increasing-subsequence.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    // Function to find length of longest increasing subsequence.
+    int longestSubsequence(vector<int>& arr) {
+        int n = arr.size();
+        vector<int>temp;
+        temp.push_back(arr[0]);
+        int len = 1;
+        for(int i = 1;i<n;i++){
+            if(arr[i] > temp.back()){
+                temp.push_back(arr[i]);
+                len++;
+            }
+            
+            else{
+                int ind = lower_bound(temp.begin(),temp.end(),arr[i])-temp.begin();
+                temp[ind] = arr[i];
+            }
+        }
+        return len;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // to ignore the newline after the integer input
+
+    while (t--) {
+        int n;
+        vector<int> arr;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            arr.push_back(num);
+
+        Solution obj;
+        cout << obj.longestSubsequence(arr) << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends

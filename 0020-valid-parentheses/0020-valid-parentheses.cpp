02272085c553dcac1121cdc80d8a707605eea8e4
Aayush Sharma <137158202+AayushSharma54652@users.noreply.github.com>
2024-12-07 23:06:22 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (19.45%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st; // Stack to store opening brackets
-        for (auto it : s) {
-            if (it == '(' || it == '[' || it == '{') {
-                st.push(it); // Push opening brackets onto the stack
-            } else {
-                if (st.empty()) return false; // If stack is empty, invalid sequence
+        stack<char>st;
+        for(auto it: s){
+            if(it == '(' || it == '[' || it == '{')st.push(it);
+            else{
+                if(st.empty())return false;
                 char ch = st.top();
                 st.pop();
-                // Check if the top of the stack matches the current closing bracket
-                if ((it == ')' && ch != '(') || 
-                    (it == '}' && ch != '{') || 
-                    (it == ']' && ch != '[')) {
+                if((it == ')' && ch != '(') || 
+                   (it == '}' && ch != '{') || 
+                   (it == ']' && ch != '[')){
                     return false;
                 }
             }
         }
-        // If the stack is empty, all brackets matched correctly
         return st.empty();
     }
-
 };
